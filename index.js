296d67ae471f5406ace9aeb8de3a86f0b5f5b037
Waheeda Afreen <82279493+WaheedaAfreen@users.noreply.github.com>
2023-04-29 20:56:29 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function updateclock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let ampm = "AM"
+    
+    if (h > 12)
+     {
+        h = h - 12;
+        ampm = "PM"
+    }
+    
+  
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+
+    hour.innerText = h;
+    minute.innerText = m;
+    seconds.innerText = s;
+    ampm.innerText = ampm;
+
+    ampm,(innerText = ampm);
+
+    
+
+    setTimeout(() => {
+        updateclock();
+    }, 1000)
+}
+updateclock();
